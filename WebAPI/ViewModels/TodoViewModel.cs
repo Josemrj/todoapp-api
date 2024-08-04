95992c3335c328b8b2c6fd56f26f8727ec79c70a
José Eduardo Jesus <jose.martins006@outlook.com>
2024-08-03 21:32:35 -0300

    insert viewmodels

---
+++ WebAPI/ViewModels/TodoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI.ViewModels
+{
+    public class TodoViewModel
+    {
+        public string Id { get; set; }
+        public DateTime DataConclusao { get; set; }
+        public string Descricao { get; set; }
+        public bool IsConcluido { get; set; }
+    }
+}
---
+++ WebAPI/ViewModels/UpdateLoteViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebAPI.ViewModels
+{
+    public class UpdateLoteViewModel
+    {
+
+        public List<Item> Itens { get; set; } = [];
+
+        public class Item
+        {
+            public string Id { get; set; }
+            public bool IsConcluido { get; set; }
+        }
+    }
+}
